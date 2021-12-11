f699f57e227bb0546e43b93c90ba83565a0aaac2
wayne <wayne@ouba.local>
2021-12-11 16:28:04 +0800

    第一个stateless widget应用

---
+++ first_statelesswidget/README.md
@@ -0,0 +1,8 @@
+# first_statelesswidget
+
+来，开发第一个stateless widget的应用
+
+## Getting Started
+
+第一个使用stateless widget的应用哟，一起来看看。
+
---
+++ first_statelesswidget/android/app/src/main/kotlin/com/flydean/first_statelesswidget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flydean.first_statelesswidget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
