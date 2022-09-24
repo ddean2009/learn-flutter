69e65c1574534bbeecb1e65e1183412dcd0de21b
wayne <ddean2009@163.com>
2022-09-24 23:33:40 +0800

    flutter系列之:flutter中listview的高级用法

---
+++ listview_app/lib/common_listview_app.dart
@@ -0,0 +1,61 @@
+
+import 'package:flutter/material.dart';
+
+
+
+class CommonListViewApp extends StatelessWidget{
+  const CommonListViewApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: List<Widget>.generate(100, (i) => Text('列表 $i')),
+    );
+  }
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: CommonListViewApp(),
+      ),
+    );
+  }
+}
---
+++ listview_app/lib/listview_builder_app.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(
+      items: List<String>.generate(10000, (i) => '列表 $i'),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  final List<String> items;
+
+  const MyApp({Key? key, required this.items}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'ListView的使用';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: ListView.builder(
+          itemCount: items.length,
+          prototypeItem: ListTile(
+            title: Text(items.first),
+          ),
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(items[index]),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
